16cbebcac66535693587a85bcabd315ef5240fa3
theo <ajavontheopro@gmail.com>
2024-07-25 03:40:09 +0200

    ✅ : Add fixture.

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -7,14 +7,22 @@ logging.basicConfig(level=logging.INFO,
                     # filemode="w",
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
-def test_root_request():
-	response = requests.get("http://127.0.0.1:2600/")
-	logging.info(response.text)
-	data = response.json()
+@pytest.fixture
+def root_response():
+	try:
+		return requests.get("http://127.0.0.1:2600/")
+	except:
+		return None
+
+def test_root_request(root_response):
+	if root_response == None:
+		return
+	logging.info(root_response.text)
+	data = root_response.json()
 	assert data["response"] == 200
 	assert data["message"] == "This is the root page."
 
 
 if __name__ == "__main__":
-	test_root_request()
+	test_root_request(requests.get("http://127.0.0.1:2600/"))
 
