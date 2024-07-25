2688ed1b0c4ee678d2cecb6477ef36dfe48fb0fb
theo <ajavontheopro@gmail.com>
2024-07-25 02:57:40 +0200

    ✅ : using async.

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def root():
+async def root():
 	return {
 		"response": 200,
 		"message": "This is the root page."
 	}
 
 @app.get("/test")
-def test():
+async def test():
 	return {
 		"response": 200,
 		"message": "This is the test page."
